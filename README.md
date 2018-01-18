84e0f45037e62dcaa8ca5ebf8df6b8abe3c365b0
Siddharth Barahalikar <barahalikar.siddharth@gmail.com>
2018-01-18 22:24:21 +0530

    inti

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # openshift-activemq
+run the application
+http://localhost:8080/produce?msg=sid123
+http://localhost:8080/receive
+
+Meanwhile check AMQ UI for \"DEMO-JMS-QUEUE"\
---
+++ src/main/java/com/javasampleapproach/jms/controller/WebController.java
@@ -0,0 +1,28 @@
+package com.javasampleapproach.jms.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javasampleapproach.jms.client.JmsClient;
+
+@RestController
+public class WebController {
+	
+	@Autowired
+	JmsClient jsmClient;
+	
+	@RequestMapping(value="/produce")
+	public String produce(@RequestParam("msg")String msg){
+		jsmClient.send(msg);
+		System.out.println("Done!! Message SENT to queue is "+msg);
+		return "Done!! Produced message is "+msg;
+	}
+	
+	@RequestMapping(value="/receive")
+	public String receive(){
+		System.out.println("Done!! Message RECEIVED from queue");
+		return jsmClient.receive();
+	}
+}
